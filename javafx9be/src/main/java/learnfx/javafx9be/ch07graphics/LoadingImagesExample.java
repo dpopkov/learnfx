4b636aef4b59eb836e9746befb317f0aca798d20
Denis Popkov <pkvdenis@gmail.com>
2019-09-08 14:34:55 +0300

    javafx9be: ch07graphics: LoadingImagesExample - refactoring.

--- javafx9be/src/main/java/learnfx/javafx9be/ch07graphics/LoadingImagesExample.java
+++ javafx9be/src/main/java/learnfx/javafx9be/ch07graphics/LoadingImagesExample.java
@@ -1,6 +1,8 @@
 package learnfx.javafx9be.ch07graphics;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,15 +25,11 @@ public class LoadingImagesExample extends Application {
         BorderPane root = new BorderPane();
         HBox box = new HBox(10);
         box.setPadding(new Insets(10));
-
-        Button loadLocalButton = new Button("Load local image");
-        loadLocalButton.setOnAction(event -> loadLocalImage());
-        Button loadClassPathImage = new Button("Load class path image");
-        loadClassPathImage.setOnAction(event -> loadClassPathImage());
-        Button loadRemoteButton = new Button("Load remote image");
-        loadRemoteButton.setOnAction(event -> loadRemoteImage());
-
-        box.getChildren().addAll(loadLocalButton, loadClassPathImage, loadRemoteButton);
+        box.getChildren().addAll(
+                makeButton("Load local image", event -> loadLocalImage()),
+                makeButton("Load class path image", event -> loadClassPathImage()),
+                makeButton("Load remote image", event -> loadRemoteImage())
+        );
         root.setTop(box);
 
         imageBox = new HBox(10);
@@ -43,43 +41,43 @@ public class LoadingImagesExample extends Application {
         stage.show();
     }
 
+    private Button makeButton(String text, EventHandler<ActionEvent> handler) {
+        Button button = new Button(text);
+        button.setOnAction(handler);
+        return button;
+    }
+
     private void loadLocalImage() {
         try {
             File file = new File("img/ch06controls/BackgroundProcesses_1.png");
             String localUrl = file.toURI().toURL().toString();
-            System.out.print("localUrl = " + localUrl);
-            System.out.println("  exists: " + file.exists());
             Image localImage = new Image(localUrl, true);
-            System.out.println("Image " + localUrl + " loading");
-            ImageView view = new ImageView(localImage);
-            imageBox.getChildren().add(view);
+            addImage(localImage);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
 
-    @SuppressWarnings("unused")
     private void loadClassPathImage() {
         final String name = "images/ch06/sportscar.png";
         URL resource = LoadingImagesExample.class.getClassLoader().getResource(name);
         if (resource != null) {
             String urlStr = resource.toExternalForm();
-            System.out.println("urlStr   = " + urlStr);
             Image classPathUrlImage = new Image(urlStr, true);
-            System.out.println("Image " + urlStr + " loading");
-            ImageView view = new ImageView(classPathUrlImage);
-            imageBox.getChildren().add(view);
+            addImage(classPathUrlImage);
         } else {
             System.err.println("Cannot load resource: " + name);
         }
     }
 
-    @SuppressWarnings("unused")
     private void loadRemoteImage() {
         String remoteUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png";
         Image remoteImage = new Image(remoteUrl, true);
-        System.out.println("Image " + remoteUrl + " loading");
-        ImageView view = new ImageView(remoteImage);
+        addImage(remoteImage);
+    }
+
+    private void addImage(Image localImage) {
+        ImageView view = new ImageView(localImage);
         imageBox.getChildren().add(view);
     }
 
